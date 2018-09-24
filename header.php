051ce0fa65d1c6ba026f7296ce5266d13a0c92e4
Marcos Di Paolo <marcosdipaolo@gmail.com>
2018-09-24 14:16:58 -0300

    maquillaje general

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 	<!-- . cabeza -->
 	<header class="cabeza">
 	    <!-- . nav -->
-	    <nav class="navbar navbar-expand-lg navbar-light">
+	    <nav class="navbar navbar-expand-lg">
 	    	<div class="container">
 	    	<a class="navbar-brand" href="<?php echo APP_URL; ?>"><img src="img/logo.png"></a>
   			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,8 +20,8 @@
 
 		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		    <ul class="navbar-nav mx-auto">
-		      <li class="nav-item active">
-		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      <li class="nav-item">
+		        <a class="nav-link" href="<?php echo APP_URL; ?>">Home <span class="sr-only">(current)</span></a>
 		      </li>
 		      <li class="nav-item">
 		        <a class="nav-link" href="productos.php">Productos</a>
