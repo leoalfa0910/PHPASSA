0dd0135e786f9eec1c0dabd5db5ccbd23fd3e1d3
nicolaslalin <nico_lalin@hotmail.com.ar>
2018-10-01 01:05:51 -0300

    se creó el faq y se corrigio el login

---
+++ faq.php
@@ -0,0 +1,44 @@
+
+<?php include 'header.php'; ?>
+
+
+<section id="faq">
+  <div class="container">
+    <div>
+      <h2> Preguntas Frecuentes </h2>
+    </div>
+    <br></br>
+    <div class="question">
+      <a class="preguntas" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Pregunta 1
+      </a>
+      <div class="collapse" id="collapseExample1">
+        <div class="card card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+    <div class="question">
+      <a class="preguntas" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Pregunta 2
+      </a>
+      <div class="collapse" id="collapseExample2">
+        <div class="card card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+    <div class="question">
+      <a class="preguntas" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Pregunta 3
+      </a>
+      <div class="collapse" id="collapseExample3">
+        <div class="card card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+
+</section>
+
+<?php include 'footer.php'; ?>
--- login.php
+++ login.php
@@ -1,63 +1,80 @@
-<?php 
 
-include 'header.php';
+<?php include_once 'autoload.php'; ?>
+    <?php include 'header.php'; ?>
 
-$errorEmail = false;
-$errorContra = false;
+    <?php
 
-if ($_POST){
+    $errorEmail = false;
+    $errorContra = false;
 
-	if (empty ($_POST['emailUsuario'])){
-		$errorEmail = true;
-	} else if (filter_var($_POST['emailUsuario'], FILTER_VALIDATE_EMAIL)===false){
-		$errorEmail = true;
-	}
+    if ($_POST){
 
-	if (empty ($_POST['contraseña'])){
-		$errorContra = true;
-	}
+      if (empty ($_POST['emailUsuario'])){
+        $errorEmail = true;
+      } else if (filter_var($_POST['emailUsuario'], FILTER_VALIDATE_EMAIL)===false){
+        $errorEmail = true;
+      }
 
-	if ($errorEmail == false && $errorContra == false){
-		header("location: ".APP_URL."/index.php"); exit();
+      if (empty ($_POST['contraseña'])){
+        $errorContra = true;
+      }
 
-	}
-}
-?>
+      if ($errorEmail == false && $errorContra == false){
+        Header ("location: ".APP_URL."/index.php"); exit();
 
-<div class="container">
-	<div class= "row justify-content-center">
-		<div class= "contenedorUno col-12 col-sm-12 col-md-4 col-lg-4 ">
-			<form action="login.php" method="post" enctype="multipart/form-data">
-				<label> Ingresá a tu cuenta </label>
-				<br></br>
-				<input type="email" placeholder="user@email.com" name="emailUsuario">
-				<?php if ($errorEmail == true )
-					echo 'Email y/o contraseña incorrecta';
-				?>
-				<br></br>
-				<label> Contraseña </label>
-				<br></br>
-				<input type="password" name="contraseña" minlength="6">
-				<?php if ($errorContra == true )
-					echo 'Email y/o contraseña incorrecta';
-				?>
+      }
+      //var_dump($_POST);
+      }
+    ?>
+    <section id="login">
+      <div class="container">
+        <div>
+          <h2 class="">Login</h2>
+        </div>
+        <div class= "row justify-content-center">
 
-				<br></br>
-				<button type="submit">Ingresá</button>
-				<button type="reset">Borrar</button>
-				<br></br>
-				<a href="contraseña.php"> Olvidé mi contraseña </a>
-				<!-- ventana a recuperar contraseña -->
-			</form>
-		</div>
+            <div class= "contenedorUno col-12 col-sm-12 col-md-4 col-lg-4 ">
+              <form action="login.php" method="post" enctype="multipart/form-data">
+                <div class="form-group">
 
-		<div class="contenedorDos col-12 col-sm-12 col-md-4 col-lg-4">
-			<a href="https://www.facebook.com" target="_blank"> Ingresar con Facebook </a>
-			<br></br>
-			<a href="registrarse.php"> ¿No tenés cuenta? </a>
-		<!-- button que te lleve a registro -->
-		</div>
-	</div>
-</div>
+                  <br><br>
+                  <label for="exampleInputEmail1">Usuario</label>
+                  <input type="email" name="emailUsuario" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="user@email.com">
+                  <small id="emailHelp" class="form-text text-muted"></small>
 
-<?php include 'footer.php'; ?>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Contraseña</label>
+                  <input type="password" name="contraseña" minlength="6" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <?php if ($errorEmail == true || $errorContra == true )
+                    echo 'Email y/o contraseña incorrecta';
+                    ?>
+                  <br><br>
+                  <button type="submit" class="btn btn-primary">Ingresá</button>
+                  <br></br>
+                  <a href="contrasenia.php"> Olvidé mi contraseña </a>
+                <!-- ventana a recuperar contraseña -->
+                </div>
+            </div>
+            <div class="contenedorDos col-12 col-sm-12 col-md-4 col-lg-4">
+                  <div class="face">
+                    <a href="https://www.facebook.com/login.php" target="_blank">
+                    <img class="logoface" src="img/facebook-login.png" alt="Facebook"></a>
+                  </div>
+
+                  <div class="cuenta">
+                    <h5> ¿No tenés cuenta? </h5>
+                  </div>
+                  <div>
+                    <a href="register.php">
+                      <button type="button" class="btn btn-terciary">Crear Cuenta</button>
+                    </a>
+                  </div>
+
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+
+      <?php include 'footer.php'; ?>
