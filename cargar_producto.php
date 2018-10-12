ae4a73242fc5a84d72ebca48c129d6e29293ab65
Marcos <marcosdipaolo@gmail.com>
2018-10-12 12:21:06 -0300

    corrijo nombre de archivo base de datos

--- cargar_producto.php
+++ cargar_producto.php
@@ -1,4 +1,4 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php'; 
 
 
 
@@ -95,8 +95,10 @@
 
 
 
+$categorias=['hardware', 'software', 'auriculares', 'teclados', 'motherboards', 'discos', 'memorias' ,'placas de video', 'sillas', 'gabinetes', 'fuentes', 'mouse'];
+$marcas=['logitech', 'redragon', 'wd', 'kingston', 'gigabyte', 'radeon', 'aerocool', 'asus', 'NZXT', 'evga', 'scandisk'];
 
-
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -105,7 +107,63 @@
 </head>
 <body>
 	<div class="container">
-		<form action=""></form>
+		<div class="row justify-content-center">
+			<div class="col-md-6">
+				<form method="POST">
+					<div class="form-group mt-4">
+						<label for="nombre">Nombre del producto</label>
+						<input class="form-control" type="text" name="nombre" id="nombre">
+					</div>
+					<div class="form-group mt-4">
+						<label for="nombre">Marca del producto</label>
+						<input class="form-control" type="text" name="marca" id="marca">
+					</div>
+					<div class="form-group mt-4">
+						<label for="nombre">Descripción del producto</label>
+						<textarea class="form-control" type="text" name="descripcion" id="descripcion"></textarea>
+					</div>
+					<div class="form-group mt-4">
+						<label for="categoria">Seleccionar Categoría</label>
+						<select class="form-control" name="categoria[]" id="categoria" multiple>
+							<option>Seleccionar categoría</option>
+							<?php 
+								foreach($categorias as $categoria) {
+									echo "<option value=\"{$categoria}\">" . ucfirst($categoria) . "</option>";
+								}
+							 ?>
+							
+						</select>
+					</div>
+
+					<div class="form-group mt-4">
+						<label for="marca">Seleccionar Marca</label>
+						<select class="form-control" name="marca" id="marca">
+							<option>Seleccionar marca</option>
+							<?php 
+								foreach($marcas as $marca) {
+									echo "<option value=\"{$marca}\">" . ucfirst($marca) . "</option>";
+								}
+							 ?>
+							
+						</select>
+					</div>
+					<div class="form-group mt-4">
+						<label for="precio">Precio del producto</label>
+						<input class="form-control" type="number" name="precio" id="precio">
+					</div>
+					<div class="form-group mt-4">
+						<label for="precio">Stock</label>
+						<input class="form-control" type="number" name="precio" id="precio">
+					</div>
+					<div class="form-group mt-4">
+						<label for="foto">Foto del producto</label><br>
+						<input type="file" name="foto" id="foto">
+					</div>
+					<button type="submit" class="btn btn-primary">Enviar</button>
+				</form>
+				<br><br>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
