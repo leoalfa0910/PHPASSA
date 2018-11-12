530d92ed17629a924cf234c8d6bd67e7ec45aa53
Marcos <marcosdipaolo@gmail.com>
2018-11-12 11:17:29 -0300

    cambié usuario y pass BD

--- Modelos/DB.php
+++ Modelos/DB.php
@@ -4,8 +4,8 @@ namespace App\Modelos;
 class DB {
 
 	static private $dsn= 'mysql:host=200.68.105.36;dbname=uv025077_proyectox';
-	static private $user = 'uv025077_marcos';
-	static private $pass = 'tE6?LV@QLtT.';
+	static private $user = 'uv025077_proyX';
+	static private $pass = '2sJ[NQRLe8xBxDVUx7r2BCpmjUr9hV@2*h?wtM62G/UbsbFErAoi({HUE[P]x';
 	static private $opt = [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION];
 	
 	public static function conectar(){
@@ -14,7 +14,7 @@ class DB {
     		$conex = new \PDO( self::$dsn, self::$user, self::$pass, self::$opt );
     		return $conex;
 
-  		} catch( PDOException $exception ){
+  		} catch( PDOException $exception ) {
 
     		$error = 'El error es: ' . $exception->getMessage();
   			return $error;
