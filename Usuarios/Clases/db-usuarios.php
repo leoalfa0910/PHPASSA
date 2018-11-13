650675d516171d2fc034471d0a308532dfa54cf9
santiosadczuk <sosadczuk@hotmail.com>
2018-11-13 09:22:52 -0300

    acomodado

--- Usuarios/Clases/db-usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once( 'autoload.php' );
-use App\Modelos\DB;
-
-$conex = DB::conectar();
-
-$sql = 'SELECT * FROM usuarios';
-$results =$conex->query($sql);
-$usuarios = $results->fetchAll(PDO::FETCH_OBJ);
